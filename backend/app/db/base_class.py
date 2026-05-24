0ec230a35f743639a47764aaf01600a6e4f1c75f
Varad Barve <varadvbarve@gmail.com>
2026-05-24 11:36:26 +0530

    bug patches in backend

--- backend/app/db/base_class.py
+++ backend/app/db/base_class.py
@@ -1,11 +1,11 @@
 from typing import Any
-from sqlalchemy.ext.declarative import as_declarative, declared_attr
+from sqlalchemy.orm import DeclarativeBase, declared_attr
 
-@as_declarative()
-class Base:
+
+class Base(DeclarativeBase):
     id: Any
     __name__: str
     # Generate __tablename__ automatically
-    @declared_attr
+    @declared_attr.directive
     def __tablename__(cls) -> str:
         return cls.__name__.lower()
